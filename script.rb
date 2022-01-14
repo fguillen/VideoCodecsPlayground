9625a9d03c7a2ef0d9dd753117931a2b9f976ba0
Fernando Guillen <fguillen.mail@gmail.com>
2022-01-14 17:52:57 +0100

    Uncommenting code

--- script.rb
+++ script.rb
@@ -13,73 +13,73 @@ class Script
   HTML_OUTPUT = "#{OUTPUT_BASE_PATH}/index.html"
 
   VARIANTS = [
-    # # Examples from here: https://trac.ffmpeg.org/wiki/Encode/VP9
-    # {
-    #   name: "Webm Average Bitrate (ABR)",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -b:v 2M -c:a libopus #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Two-Pass",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -b:v 2M -pass 1 -an -f null /dev/null && ffmpeg -i #INPUT -c:v libvpx-vp9 -b:v 2M -pass 2 -c:a libopus #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constant Quality",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -crf 30 -b:v 0 -c:a libopus #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constrained Quality",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -crf 30 -b:v 2000k -c:a libopus #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constant Bitrate",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Lossless",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -lossless 1 -c:a libopus #OUTPUT"
-    # },
+    # Examples from here: https://trac.ffmpeg.org/wiki/Encode/VP9
+    {
+      name: "Webm Average Bitrate (ABR)",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -b:v 2M -c:a libopus #OUTPUT"
+    },
+    {
+      name: "Webm Two-Pass",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -b:v 2M -pass 1 -an -f null /dev/null && ffmpeg -i #INPUT -c:v libvpx-vp9 -b:v 2M -pass 2 -c:a libopus #OUTPUT"
+    },
+    {
+      name: "Webm Constant Quality",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -crf 30 -b:v 0 -c:a libopus #OUTPUT"
+    },
+    {
+      name: "Webm Constrained Quality",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -crf 30 -b:v 2000k -c:a libopus #OUTPUT"
+    },
+    {
+      name: "Webm Constant Bitrate",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus #OUTPUT"
+    },
+    {
+      name: "Webm Lossless",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -lossless 1 -c:a libopus #OUTPUT"
+    },
 
 
-    # # Examples from here: https://trac.ffmpeg.org/wiki/Encode/H.264
-    # {
-    #   name: "MP4 Constant Quality preset slow",
-    #   container: "mp4",
-    #   video_codec: "avc1",
-    #   audio_codec: "aac",
-    #   command: "ffmpeg -i #INPUT -c:v libx264 -preset slow -crf 22 -c:a aac #OUTPUT"
-    # },
-    # {
-    #   name: "MP4 Constant Quality preset medium",
-    #   container: "mp4",
-    #   video_codec: "avc1",
-    #   audio_codec: "aac",
-    #   command: "ffmpeg -i #INPUT -c:v libx264 -preset medium -crf 22 -c:a aac #OUTPUT"
-    # },
-    # {
-    #   name: "MP4 Constant Quality preset veryslow",
-    #   container: "mp4",
-    #   video_codec: "avc1",
-    #   audio_codec: "aac",
-    #   command: "ffmpeg -i #INPUT -c:v libx264 -preset veryslow -crf 22 -c:a aac #OUTPUT"
-    # },
+    # Examples from here: https://trac.ffmpeg.org/wiki/Encode/H.264
+    {
+      name: "MP4 Constant Quality preset slow",
+      container: "mp4",
+      video_codec: "avc1",
+      audio_codec: "aac",
+      command: "ffmpeg -i #INPUT -c:v libx264 -preset slow -crf 22 -c:a aac #OUTPUT"
+    },
+    {
+      name: "MP4 Constant Quality preset medium",
+      container: "mp4",
+      video_codec: "avc1",
+      audio_codec: "aac",
+      command: "ffmpeg -i #INPUT -c:v libx264 -preset medium -crf 22 -c:a aac #OUTPUT"
+    },
+    {
+      name: "MP4 Constant Quality preset veryslow",
+      container: "mp4",
+      video_codec: "avc1",
+      audio_codec: "aac",
+      command: "ffmpeg -i #INPUT -c:v libx264 -preset veryslow -crf 22 -c:a aac #OUTPUT"
+    },
     {
       name: "MP4 Two-Pass",
       container: "mp4",
@@ -87,71 +87,71 @@ class Script
       audio_codec: "aac",
       command: "ffmpeg -y -i #INPUT -c:v libx264 -b:v 2600k -pass 1 -an -f mp4 /dev/null && ffmpeg -i #INPUT -c:v libx264 -b:v 2600k -pass 2 -c:a aac -b:a 128k #OUTPUT"
     },
-    # {
-    #   name: "MP4 Lossless H.264",
-    #   container: "mp4",
-    #   video_codec: "avc1",
-    #   audio_codec: "aac",
-    #   command: "ffmpeg -i #INPUT -c:v libx264 -c:a aac -qp 0 #OUTPUT"
-    # },
+    {
+      name: "MP4 Lossless H.264",
+      container: "mp4",
+      video_codec: "avc1",
+      audio_codec: "aac",
+      command: "ffmpeg -i #INPUT -c:v libx264 -c:a aac -qp 0 #OUTPUT"
+    },
 
-    # # Examples from here: https://gist.github.com/Vestride/278e13915894821e1d6f
+    # Examples from here: https://gist.github.com/Vestride/278e13915894821e1d6f
 
 
-    # # Other examples
-    # {
-    #   name: "MP4 no params",
-    #   container: "mp4",
-    #   video_codec: "avc1",
-    #   audio_codec: "aac",
-    #   command: "ffmpeg -i #INPUT -c:v libx264 -c:a aac #OUTPUT"
-    # },
-    # {
-    #   name: "Webm no params",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -c:a libopus #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constant Bitrate - 360p",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(360,ih)'\" #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constant Bitrate - 480p",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(480,ih)'\" #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constant Bitrate - 720p",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(720,ih)'\" #OUTPUT"
-    # },
-    # {
-    #   name: "Webm Constant Bitrate - 1080p",
-    #   container: "webm",
-    #   video_codec: "vp9",
-    #   audio_codec: "opus",
-    #   command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(1080,ih)'\" #OUTPUT"
-    # },
+    # Other examples
+    {
+      name: "MP4 no params",
+      container: "mp4",
+      video_codec: "avc1",
+      audio_codec: "aac",
+      command: "ffmpeg -i #INPUT -c:v libx264 -c:a aac #OUTPUT"
+    },
+    {
+      name: "Webm no params",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -c:a libopus #OUTPUT"
+    },
+    {
+      name: "Webm Constant Bitrate - 360p",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(360,ih)'\" #OUTPUT"
+    },
+    {
+      name: "Webm Constant Bitrate - 480p",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(480,ih)'\" #OUTPUT"
+    },
+    {
+      name: "Webm Constant Bitrate - 720p",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(720,ih)'\" #OUTPUT"
+    },
+    {
+      name: "Webm Constant Bitrate - 1080p",
+      container: "webm",
+      video_codec: "vp9",
+      audio_codec: "opus",
+      command: "ffmpeg -i #INPUT -c:v libvpx-vp9 -minrate 1M -maxrate 1M -b:v 1M -c:a libopus -filter:v \"scale=-1:'min(1080,ih)'\" #OUTPUT"
+    },
   ]
 
   def run
     puts "Starting script ..."
-    # write_csv_headers
+    write_csv_headers
 
     VARIANTS.each do |variant|
       puts "Executing variant: #{variant[:name]}"
       result = execute_variant(variant, INPUT_PATH, OUTPUT_BASE_PATH)
       log(JSON.pretty_generate(result))
-      # write_csv(result)
+      write_csv(result)
     end
     puts "End of script!"
   end
@@ -244,7 +244,7 @@ class Script
 end
 
 Script.new.run
-# Script.new.render_html
+Script.new.render_html
 
 
 
